84be979fdd569430825cb942552f2c91774fd5a8
tungarisdev <tungaris.dev@gmail.com>
2025-09-19 16:50:51 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+		// Load .env
+        Dotenv dotenv = Dotenv.load();
+        
+        // Set tất cả biến vào System properties để Spring đọc được
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
