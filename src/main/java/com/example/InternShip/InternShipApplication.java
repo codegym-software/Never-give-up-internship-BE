32022ef069deb939310e75b602913a2aa82c043e
tungarisdev <tungaris.dev@gmail.com>
2025-09-29 09:21:09 +0700

    Chinh sua env.example them thu vien dotenv

--- src/main/java/com/example/InternShip/InternShipApplication.java
+++ src/main/java/com/example/InternShip/InternShipApplication.java
@@ -3,10 +3,17 @@ package com.example.InternShip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class InternShipApplication {
 
 	public static void main(String[] args) {
+		// Load .env
+        Dotenv dotenv = Dotenv.load();
+        
+        // Set tất cả biến vào System properties để Spring đọc được
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(InternShipApplication.class, args);
 	}
 
