dbea936170b3b8394e927f2adafbc9f580cf3ba5
vunamdddddds <dochet25@gmail.com>
2025-09-29 15:50:41 +0700

    cập nhật chức năng ban , unban người dùng của admin

---
+++ src/main/java/com/example/InternShip/service/impl/AdminServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.InternShip.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.InternShip.entity.User;
+import com.example.InternShip.exception.ErrorCode;
+import com.example.InternShip.repository.UserRepository;
+import com.example.InternShip.service.AdminService;
+
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void UnBanUser(int id) {
+        // TODO Auto-generated method stub
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(ErrorCode.USER_NOT_EXISTED.getMessage()));
+        user.setActive(true);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void BanUser(int id) {
+       User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(ErrorCode.USER_NOT_EXISTED.getMessage()));
+        user.setActive(false);
+        userRepository.save(user);
+    }
+
+}
