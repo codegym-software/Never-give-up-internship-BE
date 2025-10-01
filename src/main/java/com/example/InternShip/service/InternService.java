90317f4b6c374dfe6c2db16f358525285055aa8a
vunamdddddds <dochet25@gmail.com>
2025-10-01 09:53:39 +0700

    Hoàn thành nghiệp vụ chỉnh sửa thông tin intern của vai trò hr

--- src/main/java/com/example/InternShip/service/InternService.java
+++ src/main/java/com/example/InternShip/service/InternService.java
@@ -1,5 +1,9 @@
 package com.example.InternShip.service;
 
+import com.example.InternShip.dto.request.UpdateInternRequest;
+
 public interface InternService {
+
+        void updateIntern(Integer id,UpdateInternRequest updateInternRequest);
 
 }
