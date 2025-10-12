df83ce31bb960426724764cbafe4b10adc8e7b92
namvu26112005 <128945120+vunamdddddds@users.noreply.github.com>
2025-10-12 10:15:45 +0700

    Cập nhật font chữ email gửi đi

--- src/main/java/com/example/InternShip/service/impl/PendingUserServiceImpl.java
+++ src/main/java/com/example/InternShip/service/impl/PendingUserServiceImpl.java
@@ -85,26 +85,27 @@ public class PendingUserServiceImpl implements PendingUserService {
 
                                 <h2 style="color: #1a73e8; border-bottom: 2px solid #1a73e8; padding-bottom: 10px; margin-top: 0;">Xin chào!</h2>
 
-                                <p>Cảm ơn bạn đã đăng ký. Vui lòng hoàn tất xác thực email để kích hoạt tài khoản.</p>
+                                <p>Bạn đang thực hiện hành động cần xác thực tài khoản Vui lòng hoàn tất xác thực email để xác thực tài khoản.</p>
 
                                 <p style="text-align: center; margin: 30px 0;">
                                     <a href='%s'
-                                       style="display: inline-block; padding: 12px 25px; color: #ffffff; background-color: #1a73e8; border-radius: 5px; text-decoration: none; font-weight: bold; font-size: 16px;">
+                                       style="display: inline-block; padding: 12px 25px; color: #ffffff; background-color: #1a73e8; border-radius: 5px; text-decoration: none; font-weight: b>
                                         Xác thực ngay
                                     </a>
                                 </p>
 
-                                <p>Liên kết xác thực sẽ **hết hạn sau 20 phút** tính từ: **%s**</p>
+                                <p>Liên kết xác thực sẽ hết hạn sau 20 phút tính từ: %s</p>
 
                                 <p style="font-size: 12px; color: #777;">Nếu bạn không yêu cầu xác thực email này, vui lòng bỏ qua thư này.</p>
 
                             </div>
                             <div style="text-align: center; padding: 10px 0; font-size: 12px; color: #aaa;">
-                                Bản quyền &copy; [Tên Công ty của bạn]
+                                Bản quyền &copy; CodeGym 2023.
                             </div>
                         </body>
                     </html>
-                    """.formatted(formattedNow, verifyLink);
+                    """.formatted( verifyLink,formattedNow);
+
             helper.setText(emailContent, true);
 
             mailSender.send(message);
