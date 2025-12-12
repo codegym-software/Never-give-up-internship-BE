a215ce1bc1a1a0b364c729e2b4e4b4a2d1156366
tungarisdev <tungaris.dev@gmail.com>
2025-12-12 16:16:51 +0700

    update: them res create at cho tro cap

--- src/main/java/com/example/InternShip/dto/AllowanceResponse.java
+++ src/main/java/com/example/InternShip/dto/AllowanceResponse.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +19,10 @@ public class AllowanceResponse {
     private String internshipProgramName;
     private BigDecimal amount;
     private String remiter;
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime paidAt;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime createdAt;
     private String status;
 }
