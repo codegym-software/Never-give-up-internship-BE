bf4766f15c5b1fac3dd7decde50b6a9c881716e1
vunamdddddds <dochet25@gmail.com>
2025-12-18 02:44:44 +0700

    fix đường dẫn

--- be/src/main/java/com/example/InternShip/controller/ChatController.java
+++ be/src/main/java/com/example/InternShip/controller/ChatController.java
@@ -12,6 +12,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.Map;
